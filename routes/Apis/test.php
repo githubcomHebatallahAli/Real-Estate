236f5e629060e9d8f48c15b4ff7b17954fd3aaeb
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-12-22 21:41:19 +0200

    update

--- routes/Apis/test.php
+++ routes/Apis/test.php
@@ -3,9 +3,17 @@
 
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Resources\Auth\UserRegisterResource;
+use App\Http\Controllers\Auth\VerficationController;
 
 Route::get('/test-user', function () {
     $user = User::find(7); // ابحث عن المستخدم بالـ ID المطلوب
     return new UserRegisterResource($user); // أعد البيانات في شكل JSON
 });
+Route::get('/send-sms', function () {
+    $to = '+201114990063'; // رقم المستلم
+    $message = 'Hello from Laravel using Vonage!';
+    return app()->make(VerficationController::class)->sendSms($to, $message);
+});
+
